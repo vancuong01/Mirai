0abee34550c001126b30f50d026ef3fb8463a854
Hoàng Văn Cường <cuongdzhaha@gmail.com>
2021-12-28 13:50:50 +0700

    first commit

---
+++ modules/commands/ad.js
@@ -0,0 +1,21 @@
+﻿module.exports.config = {
+	name: "adminduc",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hà Mạc Trường Giang",
+	description: "Thông tin về admin",
+	commandCategory: "Thông tin về admin",
+	cooldowns: 0
+};
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/noprefix/`;
+    if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "adminduc.gif")) request("https://media1.giphy.com/media/8lcy5k6iRvybNl1IFX/giphy.gif?cid=790b7611a72b563ba4d09c4705d96af81d87a386a57e3d0d&rid=giphy.gif&ct=g").pipe(fs.createWriteStream(dirMaterial + "adminduc.gif"));
+}
+module.exports.handleEvent = async ({ event, api, Currencies,Users, args, utils, global, client }) => {
+    const fs = require("fs");
+    let name = await Users.getNameUser(event.senderID)
+module.exports.run = ({ event, api }) => api.sendMessage(`\n★Thông Tin Admin Bot Này★\nADMIN NAME :Lê Năng Hoàng Đức\ Biệt Danh : ducngao ~~\nGiới Thiệu: Nhà anh không có gì ngoài nợ ngân hàng 10 tỉ\nI Have Something To You\nI Love You\nTính Cách : Cái gì cái chứ trêu gái là nhanh nhất nhé OwO\nChiều cao : 1m? \nSinh ngày : 24/05/200?\nLiên hệ:0345344826\nTikTok: lnhoangduc_siucapdeptrai\nSở Thích: Thích Nghịch Đủ Thứ \nCân nặng: 49kg\nUID FACEBOOK100048991791143: \nName ID Le Nang Hoang Duc \nLink Facebook : https://www.facebook.com/Mark.Zuckerberg2405\nVài lời tới người dùng BOT: Vui lòng không spam khi sử dụng để tránh die bot. Cảm ơn mọi người đã sử dụng đên con bot của mình.\nLưu ý : Đừng có dại dột mà add 2 bot kẻo bị phát hiện là bạn toang đó <3\nCảnh báo : Vui lòng không dùng bot với mục đích xấu hay cố ý report acc facebook\nChúc bạn sử dụng vui vẻ <3\n=== Le Nang Hoang Duc ===`, event.threadID, event.messageID);
+                attachment: fs.createReadStream(__dirname + `/noprefix/adminduc.gif`)
---
+++ modules/commands/taglientuc.js
@@ -0,0 +1,60 @@
+﻿module.exports.config = {
+    name: "taglientuc",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "VanHung & Dựa trên demo của NTKhang",
+    description: "Tag liên tục người bạn tag trong 5 lần\nCó thể gọi là gọi hồn người đó",
+    commandCategory: "group",
+    usages: "taglientuc @mention",
+    cooldowns: 90,
+    dependencies: {
+        "fs-extra": "",
+        "axios": ""
+    }
+}
+
+module.exports.run = async function({ api, args, Users, event}) {
+    var mention = Object.keys(event.mentions)[0];
+    if(!mention) return api.sendMessage("Cần phải tag 1 người bạn muốn gọi hồn", event.threadID);
+    let name =  event.mentions[mention];
+    var arraytag = [];
+        arraytag.push({id: mention, tag: name});
+    var a = function (a) { api.sendMessage(a, event.threadID); }
+a("R.I.P em");
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 3000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 4000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 5000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 6000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 6500);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 7000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 7500);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 8000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 8500);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 9000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 9500);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 10000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 10500);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 11000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 11500);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 12000);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 12500);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 13000);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 13500);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 14000);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 14500);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 15000);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 15500);
+setTimeout(() => {a({body: "ra đây donate vào momo 0948461545 đi" + " " + name, mentions: arraytag})} , 16000);
+setTimeout(() => {a({body: "ra đây chơi em" + " " + name, mentions: arraytag})} , 16500);
+setTimeout(() => {a({body: "ra đây chơi với Đức cute điii" + " " + name, mentions: arraytag})} , 17000);
+setTimeout(() => {a({body: "ra đây chơi với anh Đức đehp trai nào" + " " + name, mentions: arraytag})} , 17500);
+setTimeout(() => {a({body: "ra đây chơi với giẻ rách Long" + " " + name, mentions: arraytag})} , 18000);
+setTimeout(() => {a({body: "tag chết con đĩ mẹ m giờ" + " " + name, mentions: arraytag})} , 18500);
+setTimeout(() => {a({body: "ủa alo" + " " + name, mentions: arraytag})} , 19000);
+setTimeout(() => {a({body: "ra đây chơi không địt mẹ mày" + " " + name, mentions: arraytag})} , 19500);
+setTimeout(() => {a({body: "ra đây chơi em với anh đi em" + " " + name, mentions: arraytag})} , 20000);
+setTimeout(() => {a({body: "ra chơi với admin đức chim dài" + " " + name, mentions: arraytag})} , 20500);
+setTimeout(() => {a({body: "đùa à đmm" + " " + name, mentions: arraytag})} , 21000);
+setTimeout(() => {a({body: "địt cụ mày ra đây" + " " + name, mentions: arraytag})} , 21500);
+setTimeout(() => {a({body: "địt cụ mày ra đây" + " " + name, mentions: arraytag})} , 22000);
+ }
